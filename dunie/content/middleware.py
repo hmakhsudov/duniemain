3dbbd6f8ed9663e4de5c9987bf266c9325db8673
zavialova-221-365 <zavdasha2004@mail.ru>
2024-02-11 21:17:28 +0300

    making counter

--- dunie/content/middleware.py
+++ dunie/content/middleware.py
@@ -1,23 +1,34 @@
 # middleware.py
 import redis
 from django.conf import settings
+from datetime import datetime
 
 class OnlineUsersMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
         self.redis_client = redis.StrictRedis.from_url(settings.CACHES['default']['LOCATION'])
+
     def __call__(self, request):
-        user_id = request.session.session_key or self._create_session_key(request)
-        key = f'online_users:{user_id}'
+        user_ip = self._get_client_ip(request)
+        key = f'online_users:{user_ip}'
+        
         # Проверяем, был ли пользователь уже учтен
         if not self.redis_client.exists(key):
-            # Если нет, увеличиваем счетчик и устанавливаем TTL
+            # Если нет, увеличиваем счетчик
             self.redis_client.incr('online_users')
-            self.redis_client.setex(key, 60 * 5, 1)  # Устанавливаем TTL в 5 минут
+            # Устанавливаем метку, что пользователь уже учтен
+            self.redis_client.set(key, '1')
+            # Сохраняем IP-адрес и дату в отдельный список
+            timestamp = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+            self.redis_client.lpush('user_ips', f'{timestamp} - {user_ip}')
+        
         response = self.get_response(request)
         return response
 
-    def _create_session_key(self, request):
-        # Создаем уникальный идентификатор сессии, если его нет
-        request.session.create()
-        return request.session.session_key
+    def _get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        return ip
--- dunie/content/views.py
+++ dunie/content/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render
 import redis
 from django.conf import settings
 
+
 def user_count(request):
     redis_client = redis.StrictRedis.from_url(settings.CACHES['default']['LOCATION'])
     online_users = redis_client.get('online_users')
-    return render(request, 'user_count.html', {'online_users': online_users.decode('utf-8') if online_users else '0'})
+    user_ips = redis_client.lrange('user_ips', 0, -1)
+    return render(request, 'user_count.html', {'online_users': online_users.decode('utf-8') if online_users else '0', 'user_ips': user_ips})
 
 def index(request):
 
