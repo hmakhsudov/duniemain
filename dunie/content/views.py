b2aff7ca0c495bfebb62d192df1342c7be481739
zavialova-221-365 <zavdasha2004@mail.ru>
2024-02-16 21:56:58 +0300

    added db system

--- dunie/content/views.py
+++ dunie/content/views.py
@@ -38,9 +38,10 @@ def format_user_entries(user_visits):
             'city': visit.city,
         })
 
-    # Sort user entries by timestamp in reverse order (newest first)
+    # Sort user entries by the maximum timestamp across all user details in reverse order (newest first)
     user_entries.sort(key=lambda x: max(entry['timestamp'] for entry in x['user_details']), reverse=True)
 
     return user_entries
+
 def index(request):
     return render(request, 'index.html')
